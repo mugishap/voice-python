b6964ced80e073a1d74e6babb4f93c2ba3dae170
mugishap <precieuxmugisha@gmail.com>
2022-05-13 20:15:43 +0200

    snare

--- virtualvoice.py
+++ virtualvoice.py
@@ -1,9 +1,22 @@
-import pyttsx3 
+import pyttsx3
+import speech_recognition as sr
 
-friend = pyttsx3.init()
-voices =friend.getProperty('voices')
-friend.setProperty('rate',150)
-friend.setProperty('voice',voices[2].id)
-friend.say("Hello Kevin. Have you sent the file" )
-friend.runAndWait()
-friend.stop()
+r = sr.Recognizer()
+
+filename = 'no-thats-not-gonna-do-it.wav'
+
+with sr.AudioFile(filename) as source:
+
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+    print(text)
+
+    friend = pyttsx3.init()
+    voices = friend.getProperty('voices')
+    friend.setProperty('rate', 150)
+    friend.setProperty('voice', voices[2].id)
+    friend.say(text)
+    friend.runAndWait()
+    friend.stop()
+
+    
